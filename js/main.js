f6597f68033c54975de23042eb17e5cfb7875a5d
Giuliano Martinotti <giulanitto2@gmail.com>
2024-10-14 23:43:31 -0300

    Actualizacion preentrega 2

--- js/main.js
+++ js/main.js
@@ -1,205 +1,119 @@
 
-let carrito = [];
-let totaldeCompra = 0;
-let marca;
-let tipodeProducto;
-let productoSeleccionado;
+// funciones de almacenamiento de datos
 
-//log in
-let nombreyapellido = prompt("Ingrese su nombre y apellido")
-let contraseña = prompt("Ingrese su contraseña")
-
-alert("!Bienvenido " + nombreyapellido + "!")
-
-const tienda = "Padel Pro Shop"
-
-const Adidas1 = {
-    modelo: "Metalbone HRD 3.3",
-    año: 2024,
-    forma: "Diamante",
-    materiales: "Fibra de carbono aluminzado",
-    peso: "345-365",
-    eva: "High memory",
-    precio: "$340",
-}
-const Adidas2 = {
-    modelo: "Adipower Multiweight CTRL",
-    año: 2024,
-    forma: "Redonda",
-    materiales: "Carbono 18k",
-    peso: "345 - 360",
-    eva: "Goma eva",
-    precio: "$340",
-}
-const Adidas3 = {
-    modelo: "Cross It Light",
-    año: 2024,
-    forma: "Redonda",
-    materiales: "Fibra de carbono aluminizado 24k",
-    peso: "345 - 360",
-    eva: "High memory",
-    precio: "$290"
-}
-const Adidas4 = {
-    modelo: "Metalbone 3.3",
-    año: 2024,
-    forma: "Diamante Oversize",
-    materiales: "Fibra de carbono aluminzado 2 to 1",
-    peso: "345 - 365",
-    eva: "Eva soft",
-    precio: "$320",
+function almacenamientoLocal(key, valor) {
+    localStorage.setItem(key, JSON.stringify(valor))
 }
 
-
-/*const Nox1 = {
-    modelo: "AT10 Genius 18k",
-    año: 2024,
-    forma: "Gota",
-    materiales: "Fibra de carbono 18k",
-    peso: "360 - 375",
-    eva: "MLD black eva",
-    precio: "$300",
-}
-const Nox2 = {
-    modelo: "La10 Future",
-    año: 2024,
-    forma: "Diamante",
-    materiales: "Fibra de carbono 12k",
-    peso: "360 - 375",
-    eva: "HR3 core",
-    precio: "$290",
-}
-const Nox3 = {
-    modelo: "Tl10 Future Eva",
-    año: 2024,
-    forma: "Lágrima",
-    materiales: "Fibra de carbono 12k",
-    peso: "363 - 367",
-    eva: "HR3 core",
-    precio: "$265",
-}
-const Nox4 = {
-    modelo: "At10 Genius 12k",
-    año: 2024,
-    forma: "Gota",
-    materiales: "Fibra de carbono 12k",
-    peso: "360 - 370",
-    eva: "MLD black eva",
-    precio: "$280",
-}
-*/
-
-//let selecciondeProducto = prompt("¿Que marca de paleta estas buscando?")
-
-const paletasadidas = [Adidas1, Adidas2, Adidas3, Adidas4]
-/*const paletasnox = [Nox1, Nox2, Nox3, Nox4]*/
-
-for (const paletasadida of paletasadidas) {
-    console.log("modelo: " + paletasadida.modelo)
+function obtenerAlmacenamientoLocal(key) {
+    const datos = JSON.parse(localStorage.getItem(key))
+    return datos
 }
 
-/*for (const paletasno of paletasnox) {
-    console.log ("modelo: "+ paletasno.modelo)
-}*/
+let productos = obtenerAlmacenamientoLocal("productos") || [];
+
+//constantes
+const informacionDeCompra = document.getElementById("informacionDeCompra");
+const contenedorDeCompra = document.getElementById("contenedorDeCompra");
+const compraDeProductos = document.getElementById("compraDeProductos");
+const contenedorC = document.getElementById("contenedorC");
+const carrito = document.getElementById("carrito");
+const header = document.querySelector("#header");
+const numeroDelCarrito = document.getElementById("numeroDelCarrito");
+const total = document.getElementById("total");
+const x = document.getElementById("x");
+const body = document.querySelector("#body");
+
+//variables
+
+let lista = []
+let valortotal = 0
 
 
-let selecciondeProducto = prompt("¿Que modelo de paleta estas buscando?")
+window.addEventListener("scroll", function () {
+    if (contenedorC.getBoundingClientRect().top < 10) {
+        header.classList.add("scroll")
+    }
+    else {
+        header.classList.remove("scroll")
+    }
+})
+window.addEventListener('load', () => {
+    visualizarProductos();
+    contenedorDeCompra.classList.add("none")
+})
 
-if (selecciondeProducto == "Adidas1"){
-    console.table(Adidas1),
-    marca = Adidas1
-} else if (selecciondeProducto == "Adidas2"){
-    console.table(Adidas2)
-    marca = Adidas2
-} else if (selecciondeProducto == "Adidas3"){
-    console.table(Adidas3)
-    marca = Adidas3
-} else if (selecciondeProducto == "Adidas4"){
-    console.table(Adidas4)
-    marca = Adidas4
-} else {
-    alert ("Actualmente no tenemos stock de modelos " + selecciondeProducto + ", puedes consultar más tarde! Gracias por visitar " + tienda)
-}
-
-
-/*const agregaralcarrito = () => {
-    let finalizarcompra = prompt ("Desea agregar el modelo " + productoSeleccionado.modelo + " al carrito? \n (confirmar/cancelar)").toLowerCase()
-
-    if(finalizarcompra == "confirmar"){
-        carrito.push(productoSeleccionado)
-        totaldeCompra = totaldeCompra + productoSeleccionado.precio
-        console.table (carrito)
-    } else if (finalizarcompra === "cancelar"){
-        console.log ("Se canceló la compra.")
-    } else {
-        alert ("Siga comprando!")
-        agregaralcarrito()
+function visualizarProductos() {
+    contenedorC.innerHTML = ""
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].existencia > 0) {
+            contenedorC.innerHTML += `<div><imgjv src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><button onclick=comprar(${i})>Comprar</button></div></div>`
+        }
+        else {
+            contenedorC.innerHTML += `<div><imgjv src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><p class="soldOut">Sold Out</p></div></div>`
+        }
     }
 }
-*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function comprar(indice) {
+    lista.push({ nombre: productos[indice].nombre, precio: productos[indice].valor })
+    let van = true
+    let i = 0
+    while (van == true) {
+        if (productos[i].nombre == productos[indice].nombre) {
+            productos[i].existencia -= 1
+            if (productos[i].existencia == 0) {
+                visualizarProductos()
+            }
+            van = false
+        }
+        guardarAlmacenamientoLocal("productos", productos)
+        i += 1
+    }
+    numeroDelCarrito.innerHTML = lista.length
+    numeroDelCarrito.classList.add("diseñoNumero")
+    return lista
+}
+carrito.addEventListener("click", function () {
+    body.style.overflow = "hidden"
+    contenedorDeCompra.classList.remove('none')
+    contenedorDeCompra.classList.add('contenedorCompra')
+    informacionDeCompra.classList.add('informacionCompra')
+    mostrarElemtrosLista()
+})
+
+function mostrarElemtrosLista() {
+    productosCompra.innerHTML = ""
+    valortotal = 0
+    for (let i = 0; i < lista.length; i++) {
+        productosCompra.innerHTML += `<div><div class="img"><button onclick=eliminar(${i}) class="botonTrash"><img src="/img/trash.png"></button><p>${lista[i].nombre}</p></div><p> $${lista[i].precio}</p></div>`
+        valortotal += parseInt(lista[i].precio)
+    }
+    total.innerHTML = `<p>Valor Total</p> <p><span>$${valortotal}</span></p>`
+}
+
+function eliminar(indice) {
+    let van = true
+    let i = 0
+    while (van == true) {
+        if (productos[i].nombre == lista[indice].nombre) {
+            productos[i].existencia += 1
+            lista.splice(indice, 1)
+            van = false
+        }
+        i += 1
+    }
+    guardarAlmacenamientoLocal("productos", productos)
+    numeroDelCarrito.innerHTML = lista.length
+    if (lista.length == 0) {
+        numeroDelCarrito.classList.remove("diseñoNumero")
+    }
+    visualizarProductos()
+    mostrarElemtrosLista()
+}
+x.addEventListener("click", function () {
+    body.style.overflow = "auto"
+    contenedorDeCompra.classList.add('none')
+    contenedorDeCompra.classList.remove('contenedorCompra')
+    informacionDeCompra.classList.remove('informacionCompra')
+})
